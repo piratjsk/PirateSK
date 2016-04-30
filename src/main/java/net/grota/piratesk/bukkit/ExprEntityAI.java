e439ec9ce1afbd9faf7a6fb6793753c708f99e27
piratjsk <piratjsk@gmail.com>
2016-04-30 16:47:01 +0200

    small fix to 'ai' expr

--- src/main/java/net/grota/piratesk/bukkit/ExprEntityAI.java
+++ src/main/java/net/grota/piratesk/bukkit/ExprEntityAI.java
@@ -13,24 +13,25 @@ public class ExprEntityAI extends SimplePropertyExpression<LivingEntity, Boolean
     }
 
     public Class<?>[] acceptChange(final ChangeMode mode) {
-        if (mode == ChangeMode.SET || mode == ChangeMode.DELETE || mode == ChangeMode.RESET || mode == ChangeMode.REMOVE_ALL)
+        if (mode == ChangeMode.SET || mode == ChangeMode.DELETE || mode == ChangeMode.RESET)
             return new Class[] {Boolean.class};
         return null;
     }
 
     public void change(final Event e, @Nullable final Object[] delta, final ChangeMode mode) {
-        if (delta == null) {
-            for (final LivingEntity en : getExpr().getArray(e)) {
+        if (mode == ChangeMode.DELETE) {
+            for (final LivingEntity en : getExpr().getArray(e))
                 en.setAI(false);
-            }
+        } else if (mode == ChangeMode.RESET) {
+            for (final LivingEntity en : getExpr().getArray(e))
+                en.setAI(true);
         } else {
             final boolean ai = (Boolean) delta[0];
-            for (final LivingEntity en : getExpr().getArray(e)) {
+            for (final LivingEntity en : getExpr().getArray(e))
                 en.setAI(ai);
-            }
         }
     }
-    
+
     protected String getPropertyName() {
         return "ai";
     }
